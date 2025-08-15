ff88d0106d96539e1f7c9504163486d054ec5141
Farris <maymyatthaw61005@gmail.com>
2025-08-16 02:07:16 +0630

    Update MBTI

--- src/main/java/com/example/mbti/controller/PageController.java
+++ src/main/java/com/example/mbti/controller/PageController.java
@@ -2,21 +2,32 @@ package com.example.mbti.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class PageController {
 
     @GetMapping("/")
-    public String index() {
-        return "index";  // index.html
+    public String home(Model model) {
+        model.addAttribute("title", "Home - ITBuddy");
+        return "home";
     }
 
     @GetMapping("/about")
-    public String about() {
-        return "about";  // about.html
+    public String about(Model model) {
+        model.addAttribute("title", "About - ITBuddy");
+        return "about";
     }
 
+    @GetMapping("/mbti")
+    public String mbti(Model model) {
+        model.addAttribute("title", "MBTI Description - ITBuddy");
+        return "mbtiDescription";
+    }
 }
+
+
+
 
     
 

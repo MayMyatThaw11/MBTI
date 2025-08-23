8c8491126475fd05837330ee362615a7d25083ab
Farris <maymyatthaw61005@gmail.com>
2025-08-24 01:22:35 +0630

    navigation finished

--- src/main/java/com/example/mbti/controller/PageController.java
+++ src/main/java/com/example/mbti/controller/PageController.java
@@ -1,4 +1,4 @@
-package com.example.mbti.controller;
+/*package com.example.mbti.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,16 +7,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
 
     @GetMapping("/")
-    public String index() {
-        return "index";  // index.html
+    public String Home() {
+        return "Home";  // Home.html
     }
 
-    @GetMapping("/about")
-    public String about() {
-        return "about";  // about.html
+    @GetMapping("/MbtiDescription")
+    public String MbtiDescription() {
+        return "MbtiDescription";  // MBTIdescription.html
     }
 
+    @GetMapping("/feature")
+    public String feature() {
+        return "feature";  // feature.html
+    }
 }
+*/
 
-    
 
+package com.example.mbti.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String Home(Model model) {
+        model.addAttribute("page", "home");
+        return "Home";  // Home.html
+    }
+
+    @GetMapping("/MbtiDescription")
+    public String MbtiDescription(Model model) {
+        model.addAttribute("page", "mbti");
+        return "MbtiDescription";  // MBTIdescription.html
+    }
+
+    @GetMapping("/feature")
+    public String feature(Model model) {
+        model.addAttribute("page", "feature");
+        return "feature";  // feature.html
+    }
+}
